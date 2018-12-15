895b38a8cfeb685b1ee0504244fe4affc196f63c
anuj <anujjangra25119@gmail.com>
2018-12-15 16:49:55 +0530

    added paper selection components

--- src/app/mainapp/papers/papers-routing.module.ts
+++ src/app/mainapp/papers/papers-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaperViewComponent } from './paper-view/paper-view.component';
 import { UploadComponent } from './upload/upload.component';
+import { CourseComponent } from './course/course.component';
+import { BranchComponent } from './branch/branch.component';
+import { SemesterComponent } from './semester/semester.component';
 
 const routes: Routes = [
+  { path: ':course', component: CourseComponent },
+  { path: ':course/:branch', component: BranchComponent },
+  { path: ':course/:branch/:sem', component: SemesterComponent },
   { path: ':course/:branch/:sem/:code', component: PaperViewComponent },
   { path: 'upload', component: UploadComponent },
 ];
