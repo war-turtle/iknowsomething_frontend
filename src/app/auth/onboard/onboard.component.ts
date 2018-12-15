9801bb65725125a8af11bc871171f94bde876f25
war-turtle <kartikyadav71199@gmail.com>
2018-12-15 18:47:35 +0530

    onboard functionalities added

--- src/app/auth/onboard/onboard.component.ts
+++ src/app/auth/onboard/onboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthServiceClass } from '../auth.service';
+
+declare const M;
 
 @Component({
   selector: 'app-onboard',
@@ -9,15 +12,29 @@ import { AuthServiceClass } from '../auth.service';
 })
 export class OnboardComponent implements OnInit {
 
-  constructor(private AuthService: AuthServiceClass) { }
+  constructor(private onboardService: AuthServiceClass, private router: Router) { }
 
   ngOnInit() {
-
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
-    // this.AuthService.onBoard()
+    if (form.invalid) {
+      M.toast({ html: 'Enter valid data in the form' });
+    } else {
+      const data = {
+        token: window.sessionStorage.getItem('token'),
+        ...form.value
+      };
+      console.log(data);
+
+      this.onboardService.onBoard(data)
+        .subscribe((res) => {
+          M.toast({ html: 'Boarding you to the land of immortals' });
+          // this.router.navigate(['']);
+        }, (err) => {
+          M.toast({ html: err.message });
+        });
+    }
   }
 
 }
