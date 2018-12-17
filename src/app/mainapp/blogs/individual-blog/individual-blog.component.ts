f8fb16b91b841819a44f5131c13c2f5bb25ba48d
anuj <anujjangra25119@gmail.com>
2018-12-18 00:09:10 +0530

    added individual blog page

--- src/app/mainapp/blogs/individual-blog/individual-blog.component.ts
+++ src/app/mainapp/blogs/individual-blog/individual-blog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogsServiceClass } from '../blogs.service';
+import { response } from 'src/app/shared/response.model';
 
 @Component({
   selector: 'app-individual-blog',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./individual-blog.component.scss']
 })
 export class IndividualBlogComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogsServiceClass
+  ) {}
 
-  constructor() { }
+  blogId: string;
+  blog: any;
 
   ngOnInit() {
+    this.blogId = this.route.snapshot.paramMap.get('id'); // getting id from url params
+    this.blogService.getBlog(this.blogId).subscribe((res: response) => {
+      this.blog = res.data.blog;
+      console.log(this.blog);
+    });
   }
-
 }
